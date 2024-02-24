0fe9a2d554d3e8c03c9fc0227fab5a38a8812cc5
mbappai <mujahid.bappai@yahoo.com>
2024-02-24 16:37:24 +0100

    refactor: fix formatting

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,12 +11,12 @@ export default function Home({ posts }) {
       <div className="">
 
         <div className=' mt-24 flex flex-col gap-y-6 w-full max-w-3xl'>
-          <h1 className="text-lg font-medium tracking-normal">Welcome to my Digital Garden</h1>  
-          <p className="text-lg font-medium tracking-normal">My name is Mujahid Bappai, a professional virtual assistant on the web. This is the little space I carved up for myself on the web for me to dump my thoughts</p>
+          <h1 className="text-lg font-medium tracking-normal">Welcome to my Digital Garden</h1>
+          <p className="text-lg font-medium tracking-normal">My name is Mujahid Bappai. This is the little space I carved up for myself on the web for me to dump my thoughts</p>
           <p className="text-lg font-medium tracking-normal">This is not at all fancy or anything</p>
           <p className="text-lg font-medium tracking-normal">All articles or post you see on this page might or might not be fully formed so please take everything with a grain of salt</p>
         </div>
-        
+
         <div className=" mt-24 ">
           <h2 className='text-md mb-8 font-bold tracking-wider uppercase'>Brain dumps</h2>
           <ul >
@@ -27,7 +27,7 @@ export default function Home({ posts }) {
                 <li key={slug} className="py-4">
                   <article>
                     <div className="xl:items-baseline xl:space-y-0">
-                    
+
                       <dl className='sr-only'>
                         <dt className="sr-only">Published on</dt>
                         <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
@@ -37,16 +37,16 @@ export default function Home({ posts }) {
 
                       <div className="space-y-1 xl:col-span-3">
 
-                          <div>
-                            <h3 className="text-lg underline decoration-1 underline-offset-2 font-medium tracking-normal">
-                              <Link
-                                href={`/blog/${slug}`}
-                                className="text-gray-900 dark:text-gray-100"
-                              >
-                                {title}
-                              </Link>
-                            </h3>
-                          </div>
+                        <div>
+                          <h3 className="text-lg underline decoration-1 underline-offset-2 font-medium tracking-normal">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h3>
+                        </div>
 
                         {/* <div className="text-base font-medium leading-6">
                           <Link
@@ -66,7 +66,7 @@ export default function Home({ posts }) {
           </ul>
         </div>
 
-       </div>
+      </div>
 
       {posts.length > MAX_DISPLAY && (
         <div className="flex justify-end text-base font-medium leading-6">
